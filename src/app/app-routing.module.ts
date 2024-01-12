c3750485e2ad2036b8873887e59fd32750fa00fa
salmanMohammed2001 <salmannisthar123@gmail.com>
2024-01-12 19:50:24 +0530

    create home module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import {LoginComponent} from "./modules/security/components/login/login.component";
 
 const routes: Routes = [
-
-  {path:'login',component:LoginComponent},
-  {path:'',redirectTo:'login',pathMatch:'full'},
+  {path:'',redirectTo:'security/login',pathMatch:'full'},
   { path: 'security', loadChildren: () => import('./modules/security/security.module').then(m => m.SecurityModule) },
+  { path: 'console', loadChildren: () => import('./modules/console/console.module').then(m => m.ConsoleModule) },
+  { path: 'home', loadChildren: () => import('./modules/console/module/home/home.module').then(m => m.HomeModule) },
 
-  { path: 'console', loadChildren: () => import('./modules/console/console.module').then(m => m.ConsoleModule) }
 ];
 
 @NgModule({
