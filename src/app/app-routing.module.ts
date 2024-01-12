586c61b1a42dfec8766060a88c5fe96cebeafad4
salmanMohammed2001 <salmannisthar123@gmail.com>
2024-01-12 20:06:25 +0530

    create console home module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,6 @@ const routes: Routes = [
   {path:'',redirectTo:'security/login',pathMatch:'full'},
   { path: 'security', loadChildren: () => import('./modules/security/security.module').then(m => m.SecurityModule) },
   { path: 'console', loadChildren: () => import('./modules/console/console.module').then(m => m.ConsoleModule) },
-  { path: 'home', loadChildren: () => import('./modules/console/module/home/home.module').then(m => m.HomeModule) },
 
 ];
 
--- src/app/modules/console/console-routing.module.ts
+++ src/app/modules/console/console-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ConsoleComponent } from './console.component';
 
-const routes: Routes = [{ path: '', component: ConsoleComponent }];
+const routes: Routes = [
+  { path: '', component: ConsoleComponent },
+  { path: 'home', loadChildren: () => import('./module/home/home.module').then(m => m.HomeModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
