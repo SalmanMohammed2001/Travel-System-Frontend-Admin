5797566adc2c592c73a73fc8dd9ec0dd7a59f622
salmanMohammed2001 <salmannisthar123@gmail.com>
2024-01-12 10:52:50 +0530

    create login components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from "./modules/security/components/login/login.component";
 
-const routes: Routes = [{ path: 'security', loadChildren: () => import('./modules/security/security.module').then(m => m.SecurityModule) }];
+const routes: Routes = [
+
+  {path:'login',component:LoginComponent},
+  {path:'',redirectTo:'login',pathMatch:'full'},
+  { path: 'security', loadChildren: () => import('./modules/security/security.module').then(m => m.SecurityModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
