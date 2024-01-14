d8a5590889a951698918ffaf920e5799275a716e
salmanMohammed2001 <salmannisthar123@gmail.com>
2024-01-15 00:27:45 +0530

    veicle

--- src/app/modules/console/module/home/components/vehicle/inner-components/all-vehicle/all-vehicle.component.ts
+++ src/app/modules/console/module/home/components/vehicle/inner-components/all-vehicle/all-vehicle.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {VehicleService} from "../../../../../../../../service/vehicle.service";
 
 @Component({
   selector: 'app-all-vehicle',
   templateUrl: './all-vehicle.component.html',
   styleUrls: ['./all-vehicle.component.scss']
 })
-export class AllVehicleComponent {
+export class AllVehicleComponent implements OnInit {
+
+  constructor(private service: VehicleService) {
+  }
+
+  ngOnInit(): void {
+    this.findAll()
+  }
+
+
+  list: Array<any> = []
+
+  findAll() {
+    this.service.findAll().subscribe(res => {
+      this.list = res.data
+      console.log(this.list)
+    })
+  }
+  list2: Array<any> = []
+  findId(id:any){
+    this.service.findById(id).subscribe(res=>{
+      this.list2=res
+    })
+  }
 
 }
