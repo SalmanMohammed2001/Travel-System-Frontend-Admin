ea3b3d0663d9e763c51839f4dd795c1d015ebbce
salmanMohammed2001 <salmannisthar123@gmail.com>
2024-01-16 04:59:35 +0530

    delete driver ok

--- src/app/modules/console/module/home/components/driver/inner-components/delete-driver/delete-driver.component.ts
+++ src/app/modules/console/module/home/components/driver/inner-components/delete-driver/delete-driver.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {DriverService} from "../../../../../../../../service/driver.service";
 
 @Component({
   selector: 'app-delete-driver',
   templateUrl: './delete-driver.component.html',
   styleUrls: ['./delete-driver.component.scss']
 })
-export class DeleteDriverComponent {
+export class DeleteDriverComponent  implements OnInit {
+
+  constructor(private service:DriverService) {
+  }
+  ngOnInit(): void {
+    this.findAll()
+  }
+
+  list:Array<any>=[]
+
+  findAll(){
+    this.service.findAll().subscribe(res=>{
+      this.list=res.data
+      console.log(this.list)
+    })
+  }
+
+
+  delete(id:any){
+
+    if(confirm('ate you want  to delete' + id)){
+      this.service.delete(id).subscribe(res=>{
+        alert('delete')
+        this.findAll()
+      })
+    }
+
+  }
 
 }
