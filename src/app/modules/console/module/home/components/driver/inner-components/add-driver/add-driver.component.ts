ffd717d0fdb4cb5069b0b875f51372b0ef100fb2
salmanMohammed2001 <salmannisthar123@gmail.com>
2024-01-15 20:57:13 +0530

    driver save ok

--- src/app/modules/console/module/home/components/driver/inner-components/add-driver/add-driver.component.ts
+++ src/app/modules/console/module/home/components/driver/inner-components/add-driver/add-driver.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {DriverService} from "../../../../../../../../service/driver.service";
 
 @Component({
   selector: 'app-add-driver',
@@ -8,7 +9,7 @@ import {FormControl, FormGroup, Validators} from "@angular/forms";
 })
 export class AddDriverComponent {
 
-  constructor() {
+  constructor(private  service:DriverService) {
   }
 
   driverImage(event:any){
@@ -37,8 +38,41 @@ export class AddDriverComponent {
   })
 
   createData(){
-    console.log(this.form)
+    let driverName=this.form.get('driverName')?.value;
+    let driverContact=this.form.get('driverContact')?.value;
+    let driverNic=this.form.get('driverNic')?.value;
+    let driverImage=this.form.get('driverImage')?.value;
+    let licenseImageFront=this.form.get('licenseImageFront')?.value;
+    let licenseImageRear=this.form.get('licenseImageRear')?.value;
+    let vehicleId=this.form.get('vehicleId')?.value;
+
+
+    console.log(driverImage)
+    console.log(licenseImageFront)
+    console.log(licenseImageRear)
+    console.log(vehicleId)
+    console.log(driverContact)
+    console.log(driverNic)
+    console.log(driverName)
+
+    const setData= new FormData()
+    setData.append('driverName',driverName!)
+    setData.append('driverContact',driverContact!)
+    setData.append('driverNic',driverNic!)
+    setData.append('driverImage',driverImage!)
+    setData.append('licenseImageFront',licenseImageFront!)
+    setData.append('licenseImageRear',licenseImageRear!)
+    setData.append('vehicleId',vehicleId!)
+
+
+
+
+    this.service.save(setData).subscribe(res=>{
+      alert('save data')
+    })
   }
+
+
 
 
 }
