b516442a14533216099ff16aa911684e0bd52a82
salmanMohammed2001 <salmannisthar123@gmail.com>
2024-01-15 20:34:19 +0530

    add  driver components form create

--- src/app/modules/console/module/home/components/driver/inner-components/add-driver/add-driver.component.ts
+++ src/app/modules/console/module/home/components/driver/inner-components/add-driver/add-driver.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-add-driver',
@@ -6,5 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-driver.component.scss']
 })
 export class AddDriverComponent {
+
+  constructor() {
+  }
+
+  driverImage(event:any){
+    let  file=event.target.files[0]
+    this.form.patchValue({driverImage:file})
+  }
+
+  licenseImageFront(event:any){
+    let  file=event.target.files[0]
+    this.form.patchValue({licenseImageFront:file})
+  }
+  licenseImageRear(event:any){
+    let  file=event.target.files[0]
+    this.form.patchValue({licenseImageRear:file})
+  }
+
+  form=new FormGroup({
+    driverName:new FormControl('',[Validators.required]),
+    driverContact:new FormControl('',[Validators.required]),
+    driverNic:new FormControl('',[Validators.required]),
+    vehicleId:new FormControl('',[Validators.required]),
+    driverImage:new FormControl('',[Validators.required]),
+    licenseImageFront:new FormControl('',[Validators.required]),
+    licenseImageRear:new FormControl('',[Validators.required]),
+
+  })
+
+  createData(){
+    console.log(this.form)
+  }
+
 
 }
