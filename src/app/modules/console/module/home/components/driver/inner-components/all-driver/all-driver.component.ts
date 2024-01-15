8362376521fcbd38d2eae0f56c477036fec9ea39
salmanMohammed2001 <salmannisthar123@gmail.com>
2024-01-16 02:19:17 +0530

    findAll driver ok

--- src/app/modules/console/module/home/components/driver/inner-components/all-driver/all-driver.component.ts
+++ src/app/modules/console/module/home/components/driver/inner-components/all-driver/all-driver.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {DriverService} from "../../../../../../../../service/driver.service";
 
 @Component({
   selector: 'app-all-driver',
   templateUrl: './all-driver.component.html',
   styleUrls: ['./all-driver.component.scss']
 })
-export class AllDriverComponent {
+export class AllDriverComponent implements OnInit {
+
+  constructor(private service:DriverService) {
+  }
+  ngOnInit(): void {
+    this.findAll()
+  }
+
+  list:Array<any>=[]
+
+  findAll(){
+    this.service.findAll().subscribe(res=>{
+      this.list=res.data
+      console.log(this.list)
+    })
+  }
+
+  list2:Array<any>=[]
+  findNic(nic:string){
+  this.service.findNic(nic).subscribe(res=>{
+    // console.log(res.data)
+    this.list2=res
+    console.log(this.list2)
+  })
+  }
+
 
 }
