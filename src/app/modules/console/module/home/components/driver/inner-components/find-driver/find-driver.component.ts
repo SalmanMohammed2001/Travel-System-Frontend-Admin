ee2c2fff6b6273844de93912a1cc136c2da679b1
salmanMohammed2001 <salmannisthar123@gmail.com>
2024-01-16 02:59:01 +0530

    find driver vehicle  ok

--- src/app/modules/console/module/home/components/driver/inner-components/find-driver/find-driver.component.ts
+++ src/app/modules/console/module/home/components/driver/inner-components/find-driver/find-driver.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {DriverService} from "../../../../../../../../service/driver.service";
 
 @Component({
   selector: 'app-find-driver',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./find-driver.component.scss']
 })
 export class FindDriverComponent {
+
+  constructor(private  service:DriverService) {
+
+  }
+
+
+
+  list:Array<any>=[]
+
+  findData(event:any){
+    this.service.findNic(event.target.value).subscribe(res=>{
+      this.list=res
+
+      console.log(this.list)
+    })
+  }
+
 
 }
