e68cda09ec8bc22c64a6a2278a40cfe254fd9358
salmanMohammed2001 <salmannisthar123@gmail.com>
2024-01-16 14:38:02 +0530

    find guide ok

--- src/app/modules/console/module/home/components/guide/inner-components/find-guide/find-guide.component.ts
+++ src/app/modules/console/module/home/components/guide/inner-components/find-guide/find-guide.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {GuideService} from "../../../../../../../../service/guide.service";
 
 @Component({
   selector: 'app-find-guide',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class FindGuideComponent {
 
+  constructor(private  service:GuideService) {
+  }
+
+  list2:Array<any>=[]
+  findGuide(event:any){
+    this.service.findById(event.target.value).subscribe(res=>{
+      this.list2=res.data
+      console.log(this.list2)
+    })
+  }
 }
