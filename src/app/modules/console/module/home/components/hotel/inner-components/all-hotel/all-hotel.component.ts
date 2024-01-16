1383f4d7d1f08c7239ea424e7e0c81da2fa1db0c
salmanMohammed2001 <salmannisthar123@gmail.com>
2024-01-17 00:51:15 +0530

    hotel findall   is ok

--- src/app/modules/console/module/home/components/hotel/inner-components/all-hotel/all-hotel.component.ts
+++ src/app/modules/console/module/home/components/hotel/inner-components/all-hotel/all-hotel.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HotelService} from "../../../../../../../../service/hotel.service";
 
 @Component({
   selector: 'app-all-hotel',
   templateUrl: './all-hotel.component.html',
   styleUrls: ['./all-hotel.component.scss']
 })
-export class AllHotelComponent {
+export class AllHotelComponent  implements OnInit{
 
+  constructor(private  service:HotelService) {
+  }
+
+  list:Array<any>=[]
+
+  ngOnInit(): void {
+  this.findAll()
+
+  }
+
+
+  findAll(){
+    this.service.findAll().subscribe(res=>{
+      this.list=res.data
+    })
+  }
+
+  list2:Array<any>=[]
+  findHotel(event:any){
+    this.service.findId(event).subscribe(res=>{
+     this.list2=res.data
+      console.log(this.list2)
+    })
+  }
 }
