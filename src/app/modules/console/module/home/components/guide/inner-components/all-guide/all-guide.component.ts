44a8913a93d865ba074aa33617f39ed0e0f8c5a2
salmanMohammed2001 <salmannisthar123@gmail.com>
2024-01-16 14:24:18 +0530

    all guide ok

--- src/app/modules/console/module/home/components/guide/inner-components/all-guide/all-guide.component.ts
+++ src/app/modules/console/module/home/components/guide/inner-components/all-guide/all-guide.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {DriverService} from "../../../../../../../../service/driver.service";
+import {GuideService} from "../../../../../../../../service/guide.service";
 
 @Component({
   selector: 'app-all-guide',
   templateUrl: './all-guide.component.html',
   styleUrls: ['./all-guide.component.scss']
 })
-export class AllGuideComponent {
+export class AllGuideComponent implements  OnInit{
+
+
+  constructor(private service:GuideService) {
+  }
+  ngOnInit(): void {
+    this.findAll()
+  }
+
+  list:Array<any>=[]
+
+  findAll(){
+    this.service.findAll().subscribe(res=>{
+      this.list=res.data
+      console.log(this.list)
+    })
+  }
+
+ list2:Array<any>=[]
+  findGuide(id:string){
+    this.service.findById(id).subscribe(res=>{
+     //  console.log(res.data)
+        this.list2=res.data
+     // console.log(this.list2)
+    })
+  }
 
 }
