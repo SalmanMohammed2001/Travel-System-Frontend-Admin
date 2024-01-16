72ee88ecf719bbcfd066229b616195be60b5d462
salmanMohammed2001 <salmannisthar123@gmail.com>
2024-01-16 19:35:00 +0530

    delete guide is  ok

--- src/app/modules/console/module/home/components/guide/inner-components/delete-guide/delete-guide.component.ts
+++ src/app/modules/console/module/home/components/guide/inner-components/delete-guide/delete-guide.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {GuideService} from "../../../../../../../../service/guide.service";
 
 @Component({
   selector: 'app-delete-guide',
   templateUrl: './delete-guide.component.html',
   styleUrls: ['./delete-guide.component.scss']
 })
-export class DeleteGuideComponent {
+export class DeleteGuideComponent implements  OnInit{
+
+  constructor(private service:GuideService) {
+  }
+
+  ngOnInit(): void {
+    this.findAll()
+  }
+
+  list:Array<any>=[]
+
+  findAll(){
+    this.service.findAll().subscribe(res=>{
+      this.list=res.data
+      console.log(this.list)
+    })
+  }
+
+
+  delete(id:string){
+    if(confirm("are you sure want to delete")){
+      this.service.delete(id).subscribe(res=>{
+        alert('delete')
+        this.findAll()
+      })
+    }
+  }
 
 }
