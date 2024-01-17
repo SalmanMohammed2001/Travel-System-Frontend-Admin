386bd3543d6936fcc9231397676059bd09dfcbf2
salmanMohammed2001 <salmannisthar123@gmail.com>
2024-01-17 06:27:05 +0530

    hotel delete ok

--- src/app/modules/console/module/home/components/hotel/inner-components/delete-hotel/delete-hotel.component.ts
+++ src/app/modules/console/module/home/components/hotel/inner-components/delete-hotel/delete-hotel.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HotelService} from "../../../../../../../../service/hotel.service";
 
 @Component({
   selector: 'app-delete-hotel',
   templateUrl: './delete-hotel.component.html',
   styleUrls: ['./delete-hotel.component.scss']
 })
-export class DeleteHotelComponent {
+export class DeleteHotelComponent implements OnInit{
 
+  constructor(private service:HotelService) {
+  }
+
+  list:Array<any>=[]
+
+  ngOnInit(): void {
+    this.findAll()
+
+  }
+
+
+  findAll(){
+    this.service.findAll().subscribe(res=>{
+      this.list=res.data
+    })
+  }
+
+
+  deleteHotel(id:any){
+
+    if(confirm('ate you want  to delete' + id)){
+      this.service.delete(id).subscribe(res=>{
+        alert('delete')
+        this.findAll()
+      })
+    }
+
+  }
 }
