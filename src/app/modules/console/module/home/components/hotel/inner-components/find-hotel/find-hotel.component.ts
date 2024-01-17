1db984f9c502a5e58e075f6d717a042a51d9a93c
salmanMohammed2001 <salmannisthar123@gmail.com>
2024-01-17 05:39:47 +0530

    hotel findId   is ok

--- src/app/modules/console/module/home/components/hotel/inner-components/find-hotel/find-hotel.component.ts
+++ src/app/modules/console/module/home/components/hotel/inner-components/find-hotel/find-hotel.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HotelService} from "../../../../../../../../service/hotel.service";
 
+// @ts-ignore
 @Component({
   selector: 'app-find-hotel',
   templateUrl: './find-hotel.component.html',
   styleUrls: ['./find-hotel.component.scss']
 })
-export class FindHotelComponent {
+export class FindHotelComponent  implements OnInit{
 
+  constructor(private  service:HotelService) {
+  }
+
+
+
+
+
+  list2:Array<any>=[]
+  findHotel(event:any){
+    this.service.findId(event.target.value).subscribe(res=>{
+      this.list2=res.data
+      console.log(this.list2)
+    })
+  }
+
+
+
+  ngOnInit(): void {
+  }
 }
