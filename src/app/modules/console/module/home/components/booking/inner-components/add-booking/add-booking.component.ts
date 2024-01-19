768649a3ecff5dd2b077bd0cd05ca026e7facd52
salmanMohammed2001 <salmannisthar123@gmail.com>
2024-01-19 12:12:45 +0530

    load packageDetails combobox and list

--- src/app/modules/console/module/home/components/booking/inner-components/add-booking/add-booking.component.ts
+++ src/app/modules/console/module/home/components/booking/inner-components/add-booking/add-booking.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {NgbDateStruct} from "@ng-bootstrap/ng-bootstrap";
 import {CustomerService} from "../../../../../../../../service/customer.service";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {PackageDetailsService} from "../../../../../../../../service/package-details.service";
 
 
 @Component({
@@ -10,11 +12,12 @@ import {CustomerService} from "../../../../../../../../service/customer.service"
 })
 export class AddBookingComponent implements  OnInit{
 
-  constructor(private  customerService:CustomerService) {
+  constructor(private  customerService:CustomerService,private packageService:PackageDetailsService) {
   }
 
   ngOnInit(): void {
-    this.findAllCustomer()
+    this.AllCustomerDetails()
+    this.AllPackageDetail()
   }
 
 
@@ -27,22 +30,83 @@ export class AddBookingComponent implements  OnInit{
   }
 
   customerList:Array<any>=[]
-  findAllCustomer  (){
+  AllCustomerDetails  (){
     this.customerService.findAll().subscribe(res=>{
       this.customerList=res.data
-      console.log(this.customerList)
+
     })
   }
 
   customerData=''
+  packageData=''
 
+  // @ts-ignore
   loadAllCustomer(event:any){
+
     let email=event.target.value
 
+    if(email ==="null"){
+      return ""
+    }else {
+      this.customerService.findEmail(email).subscribe(res=>{
+       this.form.patchValue({
+         customerName:res[0].username,
+         customerContact:res[0].contact.contact1,
+         customerAddress:res[0].address,
 
-     this.customerService.findEmail(email).subscribe(res=>{
-       console.log(res)
-     })
+       })
+      })
+    }
+
   }
 
+  packageDetailsList:Array<any>=[]
+  AllPackageDetail(){
+    this.packageService.findAll().subscribe(res=>{
+      this.packageDetailsList=res.data
+      console.log(this.packageDetailsList)
+    })
+  }
+
+  findPackageList:Array<any>=[]
+  // @ts-ignore
+  loadAllPackage(event:any){
+  let  packageId=event.target.value
+
+    if(packageId ==="null"){
+      return ""
+    }else {
+      this.packageService.findId(packageId).subscribe(res=>{
+        this.findPackageList=res.data
+        console.log(this.findPackageList)
+      })
+    }
+
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  form=new FormGroup({
+    customerName: new FormControl('', [Validators.required]),
+    customerContact: new FormControl('', [Validators.required]),
+    customerAddress: new FormControl('', [Validators.required]),
+  })
 }
